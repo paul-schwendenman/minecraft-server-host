20053b136d63a8aed5b6dc541b59917d94517988
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-03-09 00:46:34 -0400

    Add basic routing

--- api/lambda_handler.py
+++ api/lambda_handler.py
@@ -1,7 +1,15 @@
 import json
 
 def main_handler(event, context):
+    if event["path"] == '/':
+        response_message = 'root'
+    elif event["path"] == '/stop':
+        response_message = 'stop'
+    elif event["path"] == '/start':
+        response_message = 'start'
+    else:
+        response_message = None
     return {
         "statusCode": 200,
-        "body": json.dumps('Cheers from AWS Lambda!!')
+        "body": json.dumps(response_message)
     }
