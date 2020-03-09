2d54b6bbe843934ea3dc2590a36e27d5f5db21f8
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-03-08 23:12:02 -0400

    Add API gateway configuration
    Source: https://learn.hashicorp.com/terraform/aws/lambda-api-gateway

--- lambda.tf
+++ lambda.tf
@@ -45,6 +45,61 @@ resource "aws_iam_role" "lambda_exec" {
 EOF
 }
 
+resource "aws_api_gateway_rest_api" "minecraft_gateway" {
+  name        = "MinecraftAPI"
+  description = "Minecraft Management Application"
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.minecraft_gateway.id
+  parent_id   = aws_api_gateway_rest_api.minecraft_gateway.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.minecraft_gateway.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = aws_api_gateway_rest_api.minecraft_gateway.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
+  http_method = aws_api_gateway_method.proxy.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.minecraft_api.invoke_arn
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = aws_api_gateway_rest_api.minecraft_gateway.id
+  resource_id   = aws_api_gateway_rest_api.minecraft_gateway.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = aws_api_gateway_rest_api.minecraft_gateway.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.minecraft_api.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "dev" {
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.minecraft_gateway.id
+  stage_name  = "test"
+}
+
 output "bucket" {
   value = "${aws_s3_bucket.lambda_code.bucket}"
 }
