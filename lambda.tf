77fd9e2d03732d95a566ba8217a38d0285e450ec
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-03-08 23:16:10 -0400

    Add API Gateway to Access Lambda
    Source: https://learn.hashicorp.com/terraform/aws/lambda-api-gateway

--- lambda.tf
+++ lambda.tf
@@ -97,9 +97,24 @@ resource "aws_api_gateway_deployment" "dev" {
   ]
 
   rest_api_id = aws_api_gateway_rest_api.minecraft_gateway.id
-  stage_name  = "test"
+  stage_name  = "dev"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.minecraft_api.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The "/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.minecraft_gateway.execution_arn}/*/*"
 }
 
 output "bucket" {
   value = "${aws_s3_bucket.lambda_code.bucket}"
+}
+
+output "base_url" {
+  value = aws_api_gateway_deployment.dev.invoke_url
 }
