e8370bd06c31e302c45196d649a8e350220f3881
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-03-11 20:47:22 -0400

    Add SSL certificate
    Source: https://medium.com/runatlantis/hosting-our-static-site-over-ssl-with-s3-acm-cloudfront-and-terraform-513b799aec0f

--- dns.tf
+++ dns.tf
@@ -2,7 +2,7 @@ resource "aws_route53_zone" "primary" {
   name = var.dns_name
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "minecraft" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = var.dns_name
   type    = "A"
--- webapp.tf
+++ webapp.tf
@@ -32,3 +32,16 @@ POLICY
     error_document = "404.html"
   }
 }
+
+// Use the AWS Certificate Manager to create an SSL cert for our domain.
+// This resource won't be created until you receive the email verifying you
+// own the domain and you click on the confirmation link.
+resource "aws_acm_certificate" "certificate" {
+  // We want a wildcard cert so we can host subdomains later.
+  domain_name       = var.webapp_dns_name
+  validation_method = "DNS"
+
+  // We also want the cert to be valid for the root domain even though we'll be
+  // redirecting to the www. domain immediately.
+  subject_alternative_names = ["${var.dns_name}"]
+}
