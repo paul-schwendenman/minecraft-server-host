ec4436e5485892aa963f88af21ba6722faa6d007
Paul Schwendenman <schwendenman.paul@gmail.com>
2020-03-11 22:35:57 -0400

    Add cloudfront and fix certificate validation

--- dns.tf
+++ dns.tf
@@ -9,3 +9,15 @@ resource "aws_route53_record" "minecraft" {
   ttl     = "300"
   records = ["${aws_instance.minecraft_server.public_ip}"]
 }
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = var.webapp_dns_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.www_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.www_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
--- webapp.tf
+++ webapp.tf
@@ -40,8 +40,85 @@ resource "aws_acm_certificate" "certificate" {
   // We want a wildcard cert so we can host subdomains later.
   domain_name       = var.webapp_dns_name
   validation_method = "DNS"
+  provider = aws.virgina
 
   // We also want the cert to be valid for the root domain even though we'll be
   // redirecting to the www. domain immediately.
   subject_alternative_names = ["${var.dns_name}"]
+}
+
+resource "aws_cloudfront_distribution" "www_distribution" {
+  // origin is where CloudFront gets its content from.
+  origin {
+    // We need to set up a "custom" origin because otherwise CloudFront won't
+    // redirect traffic from the root domain to the www domain, that is from
+    // runatlantis.io to www.runatlantis.io.
+    custom_origin_config {
+      // These are all the defaults.
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+
+    // Here we're using our S3 bucket's URL!
+    domain_name = aws_s3_bucket.www.website_endpoint
+    // This can be any name to identify this origin.
+    origin_id   = var.webapp_dns_name
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  // All values are defaults from the AWS console.
+  default_cache_behavior {
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    // This needs to match the `origin_id` above.
+    target_origin_id       = var.webapp_dns_name
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  // Here we're ensuring we can hit this distribution using www.runatlantis.io
+  // rather than the domain name CloudFront gives us.
+  aliases = ["${var.webapp_dns_name}"]
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  // Here's where our certificate is loaded in!
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.certificate.arn
+    ssl_support_method  = "sni-only"
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  name    = aws_acm_certificate.certificate.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.certificate.domain_validation_options.0.resource_record_type
+  zone_id = aws_route53_zone.primary.zone_id
+  records = ["${aws_acm_certificate.certificate.domain_validation_options.0.resource_record_value}"]
+  ttl     = 60
+}
+
+resource "aws_route53_record" "cert_validation_alt1" {
+  name    = aws_acm_certificate.certificate.domain_validation_options.1.resource_record_name
+  type    = aws_acm_certificate.certificate.domain_validation_options.1.resource_record_type
+  zone_id = aws_route53_zone.primary.zone_id
+  records = ["${aws_acm_certificate.certificate.domain_validation_options.1.resource_record_value}"]
+  ttl     = 60
 }
