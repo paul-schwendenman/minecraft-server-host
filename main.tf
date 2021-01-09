96373f6929a566b8ef6c13627c139ef3eb73ff26
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-01-09 18:34:35 -0500

    Add required_providers to main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
+terraform {
+  required_providers {
+    archive = {
+      source = "hashicorp/archive"
+      version = "~> 1.3"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 2.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "~> 1.4"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 2.1"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "~> 2.1"
+    }
+  }
+}
+
 provider "aws" {
   region  = var.region
 }
