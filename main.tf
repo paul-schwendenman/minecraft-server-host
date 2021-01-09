3ff7f75759ef427df439be9a24845e723a40046d
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-01-09 18:14:05 -0500

    Remove deprecated provider versions
    > Warning: Version constraints inside provider configuration blocks are deprecated

--- main.tf
+++ main.tf
@@ -1,28 +1,22 @@
 provider "aws" {
-  version = "~> 2.0"
   region  = var.region
 }
 
 provider "aws" {
-  # version = "~> 2.0"
   region = "us-east-1"
   alias  = "virgina"
 }
 
 provider "tls" {
-  version = "~> 2.1"
 }
 
 provider "local" {
-  version = "~> 1.4"
 }
 
 provider "null" {
-  version = "~> 2.1"
 }
 
 provider "archive" {
-  version = "~> 1.3"
 }
 
 locals {
