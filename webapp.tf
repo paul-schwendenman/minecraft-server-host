7fd7d56098358e347e28c2d2075294bc85c4930c
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-01-10 14:41:44 -0500

    Bump AWS provider and update cert_validation to match

--- webapp.tf
+++ webapp.tf
@@ -108,19 +108,26 @@ resource "aws_cloudfront_distribution" "www_distribution" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  name    = aws_acm_certificate.certificate.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.certificate.domain_validation_options.0.resource_record_type
-  zone_id = aws_route53_zone.primary.zone_id
-  records = [aws_acm_certificate.certificate.domain_validation_options.0.resource_record_value]
-  ttl     = 60
+  for_each = {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.primary.zone_id
 }
 
-resource "aws_route53_record" "cert_validation_alt1" {
-  name    = aws_acm_certificate.certificate.domain_validation_options.1.resource_record_name
-  type    = aws_acm_certificate.certificate.domain_validation_options.1.resource_record_type
-  zone_id = aws_route53_zone.primary.zone_id
-  records = [aws_acm_certificate.certificate.domain_validation_options.1.resource_record_value]
-  ttl     = 60
+resource "aws_acm_certificate_validation" "cert_validation" {
+  provider                = aws.virgina
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
 data "archive_file" "ui_code" {
