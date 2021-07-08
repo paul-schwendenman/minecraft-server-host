19ff97f612cabbc0c0f1af28088993e4febe0c79
Paul Schwendenman <schwendenman.paul@gmail.com>
2021-07-08 12:18:16 -0400

    Format terraform files

--- main.tf
+++ main.tf
@@ -1,30 +1,30 @@
 terraform {
   required_providers {
     archive = {
-      source = "hashicorp/archive"
+      source  = "hashicorp/archive"
       version = "~> 2.0"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 3.0"
     }
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = "~> 2.0"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "~> 3.0"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
       version = "~> 3.0"
     }
   }
 }
 
 provider "aws" {
-  region  = var.region
+  region = var.region
 }
 
 provider "aws" {
