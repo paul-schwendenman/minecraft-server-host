43b5ae557685cb294ffcc2d7f19e48b082efb47e
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-09-29 23:48:19 -0400

    Fix subnet and vps ids

--- infra/test/main.tf
+++ infra/test/main.tf
@@ -1,10 +1,21 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 module "mc_stack" {
   source           = "../modules/mc_stack"
   name             = "minecraft-test"
   ami_id           = "ami-033bf9694c0d2ea06"
   instance_type    = "t3.small"
-  vpc_id           = aws_vpc.main.id
-  subnet_id        = aws_subnet.main.id
+  vpc_id           = data.aws_vpc.default.id
+  subnet_id        = tolist(data.aws_subnets.default.ids)[0]
   key_name         = "minecraft-packer"
   root_volume_size = 8
   ssh_cidr_blocks  = ["104.230.245.46/32"]
