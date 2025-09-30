e0923607e8049d66155b21ead1b82a616d5acc69
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-09-29 21:25:08 -0400

    Format project

--- infra/modules/mc_stack/main.tf
+++ infra/modules/mc_stack/main.tf
@@ -4,11 +4,11 @@ resource "aws_security_group" "minecraft" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "Minecraft"
-    from_port   = 25565
-    to_port     = 25565
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "Minecraft"
+    from_port        = 25565
+    to_port          = 25565
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
@@ -21,20 +21,20 @@ resource "aws_security_group" "minecraft" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 }
 
 resource "aws_instance" "minecraft" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
   vpc_security_group_ids = [aws_security_group.minecraft.id]
-  key_name      = var.key_name
+  key_name               = var.key_name
 
   root_block_device {
     volume_size = var.root_volume_size
