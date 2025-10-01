ca8b6396adff7cd7ec28706867ac83b9facf4cee
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-09-30 23:23:14 -0400

    Bump AMI version

--- infra/test/main.tf
+++ infra/test/main.tf
@@ -19,7 +19,7 @@ module "networking" {
 module "mc_stack" {
   source            = "../modules/mc_stack"
   name              = "minecraft-test"
-  ami_id            = "ami-033bf9694c0d2ea06"
+  ami_id            = "ami-058c197469d730229"
   instance_type     = "t3.small"
   vpc_id            = module.networking.vpc_id
   subnet_id         = module.networking.public_subnet_id
