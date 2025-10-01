71c6c1f0ea2dad38489e0b1c63d79719b80b9440
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-09-30 20:17:44 -0400

    Format module

--- infra/test/main.tf
+++ infra/test/main.tf
@@ -21,16 +21,16 @@ provider "aws" {
 }
 
 module "mc_stack" {
-  source           = "../modules/mc_stack"
-  name             = "minecraft-test"
-  ami_id           = "ami-033bf9694c0d2ea06"
-  instance_type    = "t3.small"
-  vpc_id           = data.aws_vpc.default.id
-  subnet_id        = tolist(data.aws_subnets.default.ids)[0]
-  key_name         = "minecraft-packer"
-  root_volume_size = 8
-  ssh_cidr_blocks  = ["104.230.245.46/32"]
-  world_version    = "1.21.8"
+  source            = "../modules/mc_stack"
+  name              = "minecraft-test"
+  ami_id            = "ami-033bf9694c0d2ea06"
+  instance_type     = "t3.small"
+  vpc_id            = data.aws_vpc.default.id
+  subnet_id         = tolist(data.aws_subnets.default.ids)[0]
+  key_name          = "minecraft-packer"
+  root_volume_size  = 8
+  ssh_cidr_blocks   = ["104.230.245.46/32"]
+  world_version     = "1.21.8"
   availability_zone = "us-east-2a"
 }
 
