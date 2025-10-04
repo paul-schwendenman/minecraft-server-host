e624bc3d8a3a93fdc3b524c45e661f2f046612f6
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-10-04 16:44:46 -0400

    Add optional registration

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,6 +1,10 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
+
 
 const app = mount(App, {
   target: document.getElementById('app'),
