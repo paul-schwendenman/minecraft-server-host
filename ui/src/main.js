6f460f1d915228c0fc9b1f84750716ea2ed35f8c
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-10-04 16:44:52 -0400

    Revert "Add optional registration"
    This reverts commit e624bc3d8a3a93fdc3b524c45e661f2f046612f6.

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,10 +1,6 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
-import { registerSW } from 'virtual:pwa-register'
-
-registerSW({ immediate: true })
-
 
 const app = mount(App, {
   target: document.getElementById('app'),
