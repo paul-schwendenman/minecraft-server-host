6fecdbc57c7759af0890f5000c9b920d14f55582
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-10-11 00:14:08 -0400

    Fix SPA build

--- minecraft-ui/apps/manager/src/main.js
+++ minecraft-ui/apps/manager/src/main.js
@@ -2,21 +2,9 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
-import Home from './pages/Home.svelte'
-import Maps from './pages/Maps.svelte'
-import WorldDetail from './pages/WorldDetail.svelte'
-import NotFound from './pages/NotFound.svelte'
-
-const routes = {
-  '/': Home,
-  '/maps': Maps,
-  '/maps/:world': WorldDetail,
-  '*': NotFound
-}
-
 const app = mount(App, {
   target: document.getElementById('app'),
-  props: { routes },
+  props: {},
 })
 
 export default app
