278b3cc75c884b2c834e00b4ea7b9dcc0bd6904b
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-10-15 17:01:24 -0400

    Fix mobile margin

--- minecraft-ui/apps/worlds/src/routes/+layout.svelte
+++ minecraft-ui/apps/worlds/src/routes/+layout.svelte
@@ -34,7 +34,7 @@
 		</div>
 	</nav>
 
-	<section class="mx-auto flex w-full max-w-5xl flex-1 flex-col p-4">
+	<section class="mx-auto flex w-full max-w-5xl flex-1 flex-col">
 		{@render children?.()}
 	</section>
 </main>
