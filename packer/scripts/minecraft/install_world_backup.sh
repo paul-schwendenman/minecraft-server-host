ed39550bc98dd64a1d78fc4807f763026b125e4b
Paul Schwendenman <schwendenman.paul@gmail.com>
2026-01-02 10:36:57 -0500

    Refactor backup services to use minecraftctl instead of wrapper scripts

--- packer/scripts/minecraft/install_world_backup.sh
+++ packer/scripts/minecraft/install_world_backup.sh
@@ -3,11 +3,6 @@ set -euxo pipefail
 
 SRC_DIR="/tmp/scripts/minecraft/worlds"
 
-# Backup scripts
-sudo install -Dm755 "${SRC_DIR}/backup-worlds.sh" /usr/local/bin/backup-worlds.sh
-sudo install -Dm755 "${SRC_DIR}/prune-worlds.sh" /usr/local/bin/prune-worlds.sh
-sudo install -Dm755 "${SRC_DIR}/restore-world.sh" /usr/local/bin/restore-world.sh
-
 # Backup services and timers
 sudo install -Dm644 "${SRC_DIR}/minecraft-world-backup@.service" /etc/systemd/system/minecraft-world-backup@.service
 sudo install -Dm644 "${SRC_DIR}/minecraft-world-backup@.timer" /etc/systemd/system/minecraft-world-backup@.timer
