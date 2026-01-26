83c484fbf4aa9eccd2d00862d6ef50083f7b79cc
Paul Schwendenman <schwendenman.paul@gmail.com>
2026-01-25 23:25:50 -0500

    Fix configuration

--- minecraft-ui/libs/ui/vite.config.ts
+++ minecraft-ui/libs/ui/vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { playwright } from '@vitest/browser-playwright';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
@@ -11,10 +12,9 @@ export default defineConfig({
 				extends: './vite.config.ts',
 				test: {
 					name: 'client',
-					environment: 'browser',
 					browser: {
 						enabled: true,
-						provider: 'playwright',
+						provider: playwright(),
 						instances: [{ browser: 'chromium' }]
 					},
 					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
